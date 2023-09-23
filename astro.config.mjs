a900319e72510685ac9e1946f875debc74c649b0
SuperJ4vi <aragonjavier017@gmail.com>
2023-09-22 20:35:58 -0600

    configurando astro.config.msj para github pages 6ta vez

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,3 +5,21 @@ export default defineConfig({
   site: 'https://superj4vi.github.io',
   base: '/astro-desde-cero',
 });
+
+const IsGithubPages = true
+const path = require('path')
+const folderName = path.basename(process.cwd() + "/")
+const mode = process.env.Node_ENV === "production" ? "producctio" : "development"
+const base = mode === "producctio" && IsGithubPages ? "/" + folderName : "/"
+
+module.exports = {
+  root: "src",
+  base,
+  mode,
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    assetsDir "./"
+  }
+}
+
