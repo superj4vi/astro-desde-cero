d07b5a4b204252c462c211c27230f84bf9687d18
SuperJ4vi <aragonjavier017@gmail.com>
2023-09-22 19:36:08 -0600

    configurando astro.config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  https: 'https://superj4vi.github.io/astro-desde-cero/',
+  https: 'https://superj4vi.github.io/',
   base: '/astro-desde-cero',
 });
