bb504db52435041539192454dc366e6ad7b8c041
SuperJ4vi <aragonjavier017@gmail.com>
2023-09-23 19:09:43 -0600

    configurando ghpages en astro.config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://superj4vi.github.io',
-  base: '/astro-desde-cero',
+  base: '/astro-desde-cero'
 });
 
 
